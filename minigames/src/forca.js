e50f4aef890d980c7cb40979fbed3c6130691ca2
ANA CLARA DE SOUZA CARDOSO CHAVES <ana_cs_chaves@estudante.sesisenai.org.br>
2026-06-22 21:45:49 -0300

    .

---
+++ minigames/src/forca.js
@@ -0,0 +1,72 @@
+// var = variaevl que muda
+// cont = contante, não muda
+
+const partes = [
+    `
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========`,
+    `
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========`,
+    `
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========`,
+    `
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========`,
+    `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========`,
+    `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========`,
+    `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========`,
+];
+
+
+var palavras = [
+    "banana", "coração", "computador", "celular", "cachorro"
+]
+
+var letCert = [];
+var letErr = [];
+var tent = 6;
+var vitoria = false;
+
